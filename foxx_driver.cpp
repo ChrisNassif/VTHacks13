a6e900b8213e7109ad3532a8a8449eb92fac13d6
Chris Nassif <chrisjnassif@gmail.com>
2025-09-27 15:10:05 -0400

    got button to work

--- foxx_driver.cpp
+++ foxx_driver.cpp
@@ -55,33 +55,33 @@ int NUMBER_OF_CONTROLLER_BUTTON_TYPES = sizeof(CONTROLLER_BUTTON_TYPES) / sizeof
 
 
 
-int setup_uart(const char* get_device_file_path) {
-    int fd = open(get_device_file_path, O_RDWR | O_NOCTTY | O_NONBLOCK);
+int setup_uart(const char* device_file_path) {
+    int fd = open(device_file_path, O_RDWR | O_NOCTTY | O_NONBLOCK);
     if (fd < 0) throw std::runtime_error("Failed to open UART");
 
     struct termios tty;
+    memset(&tty, 0, sizeof(tty));
     if (tcgetattr(fd, &tty) != 0) throw std::runtime_error("tcgetattr failed");
 
     cfsetospeed(&tty, B115200);
     cfsetispeed(&tty, B115200);
 
-    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8; // 8-bit chars
-    tty.c_iflag &= ~IGNBRK;                     // disable break
-    tty.c_lflag = 0;                            // no signaling chars, no echo
-    tty.c_oflag = 0;                            // no remapping, no delays
-    tty.c_cc[VMIN]  = 1;                        // read blocks for 1 byte
-    tty.c_cc[VTIME] = 1;                        // timeout 0.1s
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
+    tty.c_cflag |= (CLOCAL | CREAD);
+    tty.c_cflag &= ~(PARENB | PARODD | CSTOPB | CRTSCTS);
 
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY);     // no SW flow ctrl
-    tty.c_cflag |= (CLOCAL | CREAD);            // enable read
-    tty.c_cflag &= ~(PARENB | PARODD);          // no parity
-    tty.c_cflag &= ~CSTOPB;
-    tty.c_cflag &= ~CRTSCTS;
+    tty.c_iflag = 0;
+    tty.c_oflag = 0;
+    tty.c_lflag = 0;
+
+    tty.c_cc[VMIN]  = 1;
+    tty.c_cc[VTIME] = 0;
 
     if (tcsetattr(fd, TCSANOW, &tty) != 0) throw std::runtime_error("tcsetattr failed");
 
     return fd;
 }
+
 
 // Create a virtual gamepad via uinput
 int setup_uinput() {
@@ -138,9 +138,9 @@ void sync_events(int ufd) {
 
 
 int main() {
-    // int uart_fd = setup_uart("/dev/ttyUSB0");
+    int uart_fd = setup_uart("/dev/ttyACM1");
 
-    int uart_fd = setup_uart("/dev/F0XX");
+    // int uart_fd = setup_uart("/dev/F0XX");
     int uinput_fd = setup_uinput();
 
     int epfd = epoll_create1(0);
@@ -159,25 +159,34 @@ int main() {
 
     while (true) {
         struct epoll_event events[1];
+
         int n = epoll_wait(epfd, events, 1, -1);
 
         if (!(n > 0 && (events[0].events & EPOLLIN))) break;
         
         char uart_buffer[4];
         int len = read(uart_fd, uart_buffer, sizeof(uart_buffer));
+        // std::cout << "num:" << len << std::endl;
         
+        // for (int i = 0; i < len; i++) {
+        //     std::cout << "byte[" << i << "] = " 
+        //             << static_cast<int>(static_cast<unsigned char>(uart_buffer[i])) 
+        //             << std::endl;
+        // }
+
         // go through the UART bits and then send a key event if the bit was changed
         for (int byte_index = 0; byte_index < 4; byte_index++) {
             char changed_buttons = current_button_states[byte_index] ^ uart_buffer[byte_index];
 
             for (int bit_index = 0; bit_index < 8; bit_index++) {
                 char mask = 1 << bit_index;
-                
-                if (changed_buttons & mask) {
-                    std::cout << "bits:" << uart_buffer << std::endl;
 
-                    send_event(uinput_fd, EV_KEY, CONTROLLER_BUTTON_TYPES[byte_index * 8 + bit_index], current_button_states[byte_index] & mask);
+                if (changed_buttons & mask) {
+                    std::cout << "hi: " << (current_button_states[byte_index] & mask) << std::endl;
+                    send_event(uinput_fd, EV_KEY, CONTROLLER_BUTTON_TYPES[byte_index * 8 + bit_index], uart_buffer[byte_index] & mask);
                     sync_events(uinput_fd);
+
+                    current_button_states[byte_index] = uart_buffer[byte_index];
                 }
             }
         }
